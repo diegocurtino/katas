3750492f9cb080b091bb63e6f54c1795e601b0db
Diego Curtino <dcurtino@gmail.com>
2020-11-09 18:49:50 +0100

    Add transactionId to log messages

--- OnlineLoanQuotes/src/main/java/com/quoter/onlineloanquotes/controller/QuotationController.java
+++ OnlineLoanQuotes/src/main/java/com/quoter/onlineloanquotes/controller/QuotationController.java
@@ -20,11 +20,13 @@ import javax.validation.constraints.NotNull;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 @Validated
 public class QuotationController {
     private static final Logger LOGGER = LogManager.getLogger(QuotationController.class);
+    private static final Random RANDOM_GENERATOR = new Random();
 
     @ApiOperation(value = "Retrieve an online quote for a loan", response = Quote.class)
     @GetMapping(value = "/quote", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -36,12 +38,15 @@ public class QuotationController {
     public Quote getQuote(@ApiParam(value = "Amount requested") @RequestParam @NotNull int amountRequested)
             throws IOException, URISyntaxException {
 
+        int transactionId = RANDOM_GENERATOR.ints(0, Integer.MAX_VALUE).findFirst().getAsInt();
+        LOGGER.info("TransactionId {}. Quote request: {}", transactionId, amountRequested);
+
         UserInputValidator.validateAmountToBorrow(amountRequested);
         List<Lender> lenders = LenderFileManager.loadLendersData();
         lenders.sort(Lender::compareTo);
 
         Quote quote = new Quote(lenders, amountRequested);
-        LOGGER.info("Produced quote: {}", quote);
+        LOGGER.info("TransactionId: {}. Quote produced: {}", transactionId, quote);
         return quote;
     }
 }
